97d4eeb95e58e6eb7d4f068d7a9dc85e29cafed9
Hackbright Student <info@hackbrightacademy.com>
2015-01-21 17:11:45 -0800

    fixed to print word list

--- markov.py
+++ markov.py
@@ -5,18 +5,16 @@ import sys
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
-
-
+#    markov_dictionary = {}
+#    stop = len(words)
     for each_line in corpus: 
         stripped_line = corpus.rstrip()
         words = stripped_line.split(" ")
     print words
+#for word_index in range(0, stop - 2):
+       #markov_dictionary[(words[word_index], words[word_index + 1])] = words.get((words[word_index], words[word_index + 1]), 0) + words[word_index + 2]
+ #   print markov_dictionary
 
-
-    #markov_dictionary = {}
-    #strip = 
-    # tuples = corpus.split(' ', 2)
-    # print tuples
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
@@ -28,10 +26,9 @@ def main():
 
     # Change this to read input_text from a file
     #script, args = sys.argv
-    input_text = "Would you, could you in a house Would you, could you with a mouse Would you, could you in a box Would you, could you with a fox Would you like green eggs and ham? Would you like them, Sam I Am?"
-
-    markov_dictionary = make_chains(input_text)
-    # random_text = make_text(chain_dict)
+    input_text = "Would you, could you in"
+    chain_dict = make_chains(input_text)
+    #random_text = make_text(chain_dict)
     # print random_text
 
 if __name__ == "__main__":
